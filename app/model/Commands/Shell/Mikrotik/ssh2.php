70e3ba4daa74f69b308c3f8047c16aac44547a9d
jeger <jeger@THINK77>
2013-06-27 17:43:42 +0200

    Moved some log definitions from command class to command repository

--- app/model/Commands/Shell/Mikrotik/ssh2.php
+++ app/model/Commands/Shell/Mikrotik/ssh2.php
@@ -17,10 +17,6 @@ namespace Commands\Mikrotik;
 
 class SSH2 extends \Nette\Object {
 
-	private $deviceHost;
-	private $deviceGroupName;
-	private $username;
-	private $password;
 	private $connection = NULL;
 	private $port = 22;
 	public $lastCommand = NULL;
@@ -33,18 +29,14 @@ class SSH2 extends \Nette\Object {
 							 "server_to_client" => array("crypt" => "aes256-cbc,aes192-cbc,aes128-cbc", 
                                                         "comp" => "none")); 
 
-	private $callbacks = array("disconnect" => "disconnect_cb"); 
+	private $callbacks = array("disconnect" => "disconnect_cb");
 	
-	/** @var Log\LogRepository */
+	/** @var \Commands\ScriptCommandsRepository */
 	private $script;
 
-    public function __construct(\Commands\ScriptCommandsRepository $ScriptCommandsRepository, $deviceHost, $deviceGroupName)
+    public function __construct(\Commands\ScriptCommandsRepository $ScriptCommandsRepository)
     {
 		$this->script = $ScriptCommandsRepository;
-		$this->deviceHost = $deviceHost;
-		$this->deviceGroupName = $deviceGroupName;
-		$record = array('message' => 'Loaded class \''.get_class($this).'\'', 'severity' => 'info', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-		$this->script->log->addLog($record);
 	}
 
 	private function read($stream)
@@ -59,27 +51,28 @@ class SSH2 extends \Nette\Object {
 
 	public function connect($username,$password) 
 	{ 
-		$this->username = $username;
-		$this->password = $password;
-
-		$this->connection = ssh2_connect($this->deviceHost,$this->port,$this->methods,array($this,$this->callbacks)); 
+		$this->connection = ssh2_connect($this->script->deviceHost,$this->port,$this->methods,array($this,$this->callbacks)); 
 
 		if($this->connection) 
 		{ 
-			$record = array('message' => 'Connected', 'severity' => 'success', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-			$this->script->log->addLog($record);
-			if(!($stream = ssh2_auth_password($this->connection,$this->username,$this->password)))
+			$this->script->logRecord['message'] = 'Connected';
+			$this->script->logRecord['severity'] = 'success';
+			$this->script->log->addLog($this->script->logRecord);
+			if(!($stream = ssh2_auth_password($this->connection,$username,$password)))
 			{
-				$record = array('message' => 'User \''.$this->username.'\' login failed', 'severity' => 'error', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-				$this->script->log->addLog($record);
+				$this->script->logRecord['message'] = 'User ['.$username.'] login failed';
+				$this->script->logRecord['severity'] = 'error';
+				$this->script->log->addLog($this->script->logRecord);
 				$this->disconnect();
 			} else { 
-				$record = array('message' => 'User \''.$this->username.'\' logged in', 'severity' => 'success', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-				$this->script->log->addLog($record);
+				$this->script->logRecord['message'] = 'User ['.$username.'] logged in';
+				$this->script->logRecord['severity'] = 'success';
+				$this->script->log->addLog($this->script->logRecord);
 			} 
 		} else { 
-			$record = array('message' => 'Connection failed', 'severity' => 'error', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-			$this->script->log->addLog($record);
+			$this->script->logRecord['message'] = 'Connection failed';
+			$this->script->logRecord['severity'] = 'error';
+			$this->script->log->addLog($this->script->logRecord);
 		} 
 
 		return $this->connection; 
@@ -88,16 +81,18 @@ class SSH2 extends \Nette\Object {
 	public function disconnect_cb($reason,$message,$language) 
 	{ 
 		$this->connection = NULL;
-		$record = array('message' => 'Disconnected with reason code ['.$reason.'] and message: '.$message, 'severity' => 'warning', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-		$this->script->log->addLog($record);
+		$this->script->logRecord['message'] = 'Disconnected with reason code ['.$reason.'] and message: '.$message;
+		$this->script->logRecord['severity'] = 'warning';
+		$this->script->log->addLog($this->script->logRecord);
     } 
 
 	public function disconnect() 
 	{
 		if ($this->connection) {
 			$this->connection = NULL;
-			$record = array('message' => 'Disconnected', 'severity' => 'success', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-			$this->script->log->addLog($record);
+			$this->script->logRecord['message'] = 'Disconnected';
+			$this->script->logRecord['severity'] = 'success';
+			$this->script->log->addLog($this->script->logRecord);
 		}
     } 
 
@@ -115,33 +110,39 @@ class SSH2 extends \Nette\Object {
 			{
 				if (!preg_match($waitfor, $this->lastCommandResult))
 				{
-					$record = array('message' => 'Failed to wait for the result \''.$waitfor.'\'', 'severity' => 'error', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-					$this->script->log->addLog($record);
-					$record = array('message' => 'Failed to send the command \''.$command.'\'', 'severity' => 'error', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-					$this->script->log->addLog($record);
+					$this->script->logRecord['message'] = 'Failed to wait for the result ['.$waitfor.']';
+					$this->script->logRecord['severity'] = 'error';
+					$this->script->log->addLog($this->script->logRecord);
+					$this->script->logRecord['message'] = 'Failed to send the command ['.$command.'] with wait for ['.$waitfor.']';
+					$this->script->logRecord['severity'] = 'error';
+					$this->script->log->addLog($this->script->logRecord);
 					return FALSE;
 				} else {
-					$record = array('message' => 'Waiting for the result \''.$waitfor.'\' was successful', 'severity' => 'success', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-					$this->script->log->addLog($record);
+					$this->script->logRecord['message'] = 'Waiting for the result ['.$waitfor.'] was successful';
+					$this->script->logRecord['severity'] = 'success';
+					$this->script->log->addLog($this->script->logRecord);
 				}
 			}
 			if(!($stream = ssh2_exec($this->connection,$command,null,null,80,25)))
 			{
-				$record = array('message' => 'Failed to send command \''.$command.'\'', 'severity' => 'error', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-				$this->script->log->addLog($record);
+				$this->script->logRecord['message'] = 'Failed to send the command ['.$command.']';
+				$this->script->logRecord['severity'] = 'error';
+				$this->script->log->addLog($this->script->logRecord);
 			} else {
 				$this->lastCommand = $command;
 				$this->read($stream);
-				$record = array('message' => 'Command \''.$command.'\' sended', 'severity' => 'success', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-				$this->script->log->addLog($record);
+				$this->script->logRecord['message'] = 'Command ['.$command.'] sended';
+				$this->script->logRecord['severity'] = 'success';
+				$this->script->log->addLog($this->script->logRecord);
 				$result = explode("\n", $this->lastCommandResult);
 				array_pop($result);
 				if (count($result)) {
 					echo $result[count($result)-1];
 					if (preg_match('/error|failed|bad command/i', $result[count($result)-1])) {
 						$this->lastCommandError = $result[count($result)-1];
-						$record = array('message' => 'Command \''.$command.'\' failed with error: '.$this->lastCommandError, 'severity' => 'error', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-						$this->script->log->addLog($record);
+						$this->script->logRecord['message'] = 'Command ['.$command.'] failed with error: '.$this->lastCommandError;
+						$this->script->logRecord['severity'] = 'error';
+						$this->script->log->addLog($this->script->logRecord);
 					} else {
 						$this->lastCommandError = NULL;
 					}
@@ -153,8 +154,9 @@ class SSH2 extends \Nette\Object {
 	public function logLastCommand() {
 		if ($this->lastCommandResult)
 		{
-			$record = array('message' => 'Result of command \''.$this->lastCommand.'\': '.$this->lastCommandResult, 'severity' => 'info', 'deviceHost' => $this->deviceHost, 'deviceGroupName' => $this->deviceGroupName);
-			$this->script->log->addLog($record);
+			$this->script->logRecord['message'] = 'Result of command ['.$this->lastCommand.']: '.$this->lastCommandResult;
+			$this->script->logRecord['severity'] = 'info';
+			$this->script->log->addLog($this->script->logRecord);
 		}
 	}
 }
--- app/model/ScriptCommandsRepository.php
+++ app/model/ScriptCommandsRepository.php
@@ -18,22 +18,59 @@ use Nette;
 
 class ScriptCommandsRepository extends Nette\Object {
 
-	/** @var Log\LogRepository */
+	/** @var \Log\LogRepository */
 	public $log;
 
-    public function __construct(\Log\LogRepository $LogRepository)
+	public $logRecord;
+	public $deviceHost;
+
+	/** @var \Group\DeviceGroupRepository */
+	private $DGrepo;
+	
+	/** @var \Script\ScriptRepository */
+	private $Srepo;
+	
+    public function __construct(\Log\LogRepository $LogRepository, \Group\DeviceGroupRepository $DeviceGroupRepository, \Script\ScriptRepository $ScriptRepository)
     {
 		$this->log = $LogRepository;
+		$this->DGrepo = $DeviceGroupRepository;
+		$this->Srepo = $ScriptRepository;
     }
 	
-	public function init($class, $deviceHost, $deviceGroupName)
+	public function init($class, $deviceHost, $deviceGroupId, $scriptId)
 	{
 		$className = strtr("/Commands/".$class, "/", "\\");
+		$this->deviceHost = $deviceHost;
+		$deviceGroupName = $this->DGrepo->getDeviceGroupData(array('select' => 'groupname', 'where' => 'id='.$deviceGroupId))->fetchSingle();
+		$scriptName = $this->Srepo->getScriptData(array('select' => 'scriptName', 'where' => 'id='.$scriptId))->fetchSingle();
+		$this->logRecord = array("logId" => $this->log->getLogId(),
+								 "deviceHost" => $this->deviceHost,
+								 "deviceGroupId" => $deviceGroupId,
+								 "deviceGroupName" => $deviceGroupName,
+								 "scriptId" => $scriptId,
+								 "scriptName" => $scriptName,
+								 "class" => $className);
+
 		if(class_exists($className)) {
-			return new $className($this, $deviceHost, $deviceGroupName);
+			$_class = new $className($this);
+			if ($_class)
+			{
+				$class = str_replace("Commands\\", "", get_class($_class));
+				$this->logRecord['class'] = $class;
+				$this->logRecord['message'] = 'Loaded class ['.$class.']';
+				$this->logRecord['severity'] = 'info';
+				$this->log->addLog($this->logRecord);
+				return $_class;
+			} else {
+				$this->logRecord['message'] = 'Fail to load class ['.$class.']';
+				$this->logRecord['severity'] = 'error';
+				$this->log->addLog($this->logRecord);
+				return FALSE;
+			}
 		} else {
-			$record = array('message' => 'Class \''.$className.'\' does not exist', 'severity' => 'error', 'deviceHost' => $deviceHost, 'deviceGroupName' => $deviceGroupName);
-			$this->log->addLog($record);
+			$this->logRecord['message'] = 'Class ['.$class.'] does not exist';
+			$this->logRecord['severity'] = 'error';
+			$this->log->addLog($this->logRecord);
 			return FALSE;
 		}
 	}
