06518847907ba8eb5402eb8cd2fdc860c5945a5c
jeger <jeger@THINK77>
2013-07-24 11:57:45 +0200

    Rewritten RouterOS SSH2 library.

--- app/model/Commands/RouterOS/ssh2.php
+++ app/model/Commands/RouterOS/ssh2.php
@@ -17,21 +17,18 @@ namespace Commands\RouterOS;
 
 class SSH2 extends \Nette\Object {
 
-	private $port = 22;
-	private $fileStoragePath = '/var/backup/';
-	
 	private $connection = NULL;
-	public $lastCommand = NULL;
-	public $lastCommandResult = NULL;
-	public $lastCommandError = NULL;
-	
-	private $methods = array("kex" => "diffie-hellman-group1-sha1", 
-							 "client_to_server" => array("crypt" => "3des-cbc", 
-                                                        "comp" => "none"), 
-							 "server_to_client" => array("crypt" => "aes256-cbc,aes192-cbc,aes128-cbc", 
-                                                        "comp" => "none")); 
+	private $port = 22;
+	private $timeout = 2;
+	private $prompt = '/\[.+\] >/i';
+	private $errorPrompt = '/error|failed|bad command|expected/i';
 
-	private $callbacks = array("disconnect" => "disconnect_cb");
+	private $lastCommand = NULL;
+	private $lastCommandResult = NULL;
+	private $lastCommandError = NULL;
+	
+	const LOG_COMMAND_ERROR_SEVERITY = 'error';
+	private $logCommandErrorSeverity = self::LOG_COMMAND_ERROR_SEVERITY;
 	
 	/** @var \Commands\ScriptCommandsRepository */
 	private $script;
@@ -41,23 +38,72 @@ class SSH2 extends \Nette\Object {
 		$this->script = $ScriptCommandsRepository;
 	}
 
-	private function read($stream)
+	private function read()
 	{
-		stream_set_blocking($stream,TRUE);
-		$data = "";
-		while($buf = fread($stream,4096)) $data .= $buf;
-		fclose($stream);
-		$this->lastCommandResult = $data;
+		@$this->lastCommandResult = $this->connection->read($this->prompt, NET_SSH2_READ_REGEX);
+	}
+
+	public function setTimeout($sec) {
+		if (is_numeric($sec))
+		{
+			$this->timeout = $sec;
+			$this->script->logRecord['message'] = 'The timeout set to ['.$this->timeout.'] seconds';
+			$this->script->logRecord['severity'] = 'info';
+			$this->script->log->addLog($this->script->logRecord);
+		}
+	}
+
+	public function setPrompt($prompt) {
+		if ($prompt)
+		{
+			$this->prompt = $prompt;
+			$this->script->logRecord['message'] = 'The device prompt set to ['.$this->prompt.']';
+			$this->script->logRecord['severity'] = 'info';
+			$this->script->log->addLog($this->script->logRecord);
+		}
+	}
+
+	public function setErrorPrompt($prompt) {
+		if ($prompt)
+		{
+			$this->errorPrompt = $prompt;
+			$this->script->logRecord['message'] = 'The command error prompt set to ['.$this->errorPrompt.']';
+			$this->script->logRecord['severity'] = 'info';
+			$this->script->log->addLog($this->script->logRecord);
+		}
+	}
+
+	public function setLogError($log)
+	{
+		if ($log === '1' || strtolower($log) === 'info') {
+			$this->logCommandErrorSeverity = 'info';
+		} elseif ($log === '2' || strtolower($log) === 'warning') {
+			$this->logCommandErrorSeverity = 'warning';
+		} else {
+			$this->logCommandErrorSeverity = 'error';
+		}
+		
+		if ($log === '0' || strtolower($log) === 'false')
+		{
+			$this->logCommandErrorSeverity = FALSE;
+			$this->script->logRecord['message'] = 'Logging errors of the next command has been turned off';
+		} else {
+			$this->script->logRecord['message'] = 'Logging severity of the next command error has been set to ['.$this->logCommandErrorSeverity.']';
+		}
+		$this->script->logRecord['severity'] = 'info';
+		$this->script->log->addLog($this->script->logRecord);
 	}
 
 	public function connect($login) 
 	{ 
-		@$this->connection = ssh2_connect($this->script->deviceHost,$this->port,$this->methods,array($this,$this->callbacks)); 
+		@$this->connection = new \Net_SSH2($this->script->deviceHost, $this->port, $this->timeout);
 
-		if($this->connection)
+		if ($this->connection)
 		{ 
+			$this->connection->enablePTY();
 			$this->script->logRecord['message'] = 'Connected';
-			if ($login === '0' || strtolower($login) === 'false') $this->script->logRecord['message'] .= ' without login';
+			if ($login === '0' || strtolower($login) === 'false')
+				$this->script->logRecord['message'] .= ' without login';
 			$this->script->logRecord['severity'] = 'success';
 			$this->script->log->addLog($this->script->logRecord);
 			if (!$login || $login === '1' || strtolower($login) === 'true')
@@ -69,39 +115,42 @@ class SSH2 extends \Nette\Object {
 			$this->script->logRecord['severity'] = 'error';
 			$this->script->log->addLog($this->script->logRecord);
 		} 
-
-		return $this->connection; 
     } 
 
 	public function login() 
 	{ 
-		if($this->connection) 
+		if (!$this->connection)
+			$this->connect('true');
+
+		if ($this->connection) 
 		{ 
-			if(!(@$stream = ssh2_auth_password($this->connection,$this->script->deviceUsername,$this->script->devicePassword)))
+			if(@!$this->connection->login($this->script->deviceUsername,$this->script->devicePassword))
 			{
 				$this->script->logRecord['message'] = 'User ['.$this->script->deviceUsername.'] login failed';
 				$this->script->logRecord['severity'] = 'error';
 				$this->script->log->addLog($this->script->logRecord);
 				$this->disconnect();
-			} else { 
+			} else {
+				$this->connection->setTimeout(1);
+				$this->read();
 				$this->script->logRecord['message'] = 'User ['.$this->script->deviceUsername.'] logged in';
 				$this->script->logRecord['severity'] = 'success';
 				$this->script->log->addLog($this->script->logRecord);
 			}
+		} else {
+			$this->script->logRecord['message'] = 'Unable to login due to the connection is not established';
+			$this->script->logRecord['severity'] = 'error';
+			$this->script->log->addLog($this->script->logRecord);
 		}
-    } 
-
-	private function disconnect_cb($reason,$message,$language) 
-	{ 
-		$this->connection = NULL;
-		$this->script->logRecord['message'] = 'Disconnected with reason code ['.$reason.'] and message: '.$message;
-		$this->script->logRecord['severity'] = 'warning';
-		$this->script->log->addLog($this->script->logRecord);
     } 
 
 	public function disconnect() 
 	{
 		if ($this->connection) {
+			echo "<pre>";
+			print_r($this->connection->getLog());
+
+			$this->connection->disconnect();
 			$this->connection = NULL;
 			$this->script->logRecord['message'] = 'Disconnected';
 			$this->script->logRecord['severity'] = 'success';
@@ -109,27 +158,23 @@ class SSH2 extends \Nette\Object {
 		}
     } 
 
-	public function sleep($sec) {
-		if(is_null($sec) || !is_numeric($sec)) $sec = 5;
-		sleep($sec);
-		$this->script->logRecord['message'] = 'Sleep for ['.$sec.'] seconds';
-		$this->script->logRecord['severity'] = 'success';
-		$this->script->log->addLog($this->script->logRecord);
-	}
-	
 	public function command($command, $waitfor = NULL)
 	{
+		if (!$this->connection)
+			$this->connect('true');
+
 		if ($this->connection) {
 			if ($waitfor)
 			{
 				if (@!preg_match($waitfor, $this->lastCommandResult))
 				{
-					$this->script->logRecord['message'] = 'Failed to wait for the result ['.$waitfor.']';
-					$this->script->logRecord['severity'] = 'error';
-					$this->script->log->addLog($this->script->logRecord);
-					$this->script->logRecord['message'] = 'Failed to send the command ['.$command.'] with wait for ['.$waitfor.']';
-					$this->script->logRecord['severity'] = 'error';
-					$this->script->log->addLog($this->script->logRecord);
+					if ($this->logCommandErrorSeverity)
+					{
+						$this->script->logRecord['message'] = 'Failed to wait for the result ['.$waitfor.']. Command ['.$command.'] not sended.';
+						$this->script->logRecord['severity'] = $this->logCommandErrorSeverity;
+						$this->script->log->addLog($this->script->logRecord);
+					}
+					$this->logCommandErrorSeverity = self::LOG_COMMAND_ERROR_SEVERITY;
 					return FALSE;
 				} else {
 					$this->script->logRecord['message'] = 'Waiting for the result ['.$waitfor.'] was successful';
@@ -137,128 +182,41 @@ class SSH2 extends \Nette\Object {
 					$this->script->log->addLog($this->script->logRecord);
 				}
 			}
-			if(!(@$stream = ssh2_exec($this->connection,$command,null,null,80,25)))
+			if (!@$this->connection->exec($command))
 			{
-				$this->script->logRecord['message'] = 'Failed to send the command ['.$command.']';
-				$this->script->logRecord['severity'] = 'error';
-				$this->script->log->addLog($this->script->logRecord);
+				if ($this->logCommandErrorSeverity)
+				{
+					$this->script->logRecord['message'] = 'Failed to send the command ['.$command.']';
+					$this->script->logRecord['severity'] = $this->logCommandErrorSeverity;
+					$this->script->log->addLog($this->script->logRecord);
+				}
 			} else {
+				$this->connection->setTimeout($this->timeout);
 				$this->lastCommand = $command;
-				$this->read($stream);
+				$this->read();
+				echo "<br> >>>> ".$this->lastCommandResult." <<<< <br>";
 				$this->script->logRecord['message'] = 'Command ['.$command.'] sended';
 				$this->script->logRecord['severity'] = 'success';
 				$this->script->log->addLog($this->script->logRecord);
-				$result = explode("\n", $this->lastCommandResult);
-				array_pop($result);
-				if (count($result)) {
-					if (preg_match('/error|failed|bad command/i', $result[count($result)-1])) {
-						$this->lastCommandError = $result[count($result)-1];
+
+				@$error = preg_grep($this->errorPrompt, explode("\n", $this->lastCommandResult));
+				if (count($error))
+				{
+					$this->lastCommandError = end($error);
+					if ($this->logCommandErrorSeverity)
+					{
 						$this->script->logRecord['message'] = 'Command ['.$command.'] failed with error: '.$this->lastCommandError;
-						$this->script->logRecord['severity'] = 'error';
+						$this->script->logRecord['severity'] = $this->logCommandErrorSeverity;
 						$this->script->log->addLog($this->script->logRecord);
-					} else {
-						$this->lastCommandError = NULL;
-					}
-				}
-			}
-		}
-	}
-	
-	public function scp_recv($file) {
-		if ($this->connection)
-		{
-			$target = rtrim($this->fileStoragePath, '/').'/'.$this->script->deviceHost.'/';
-			if (!file_exists($target))
-			{
-				if (@mkdir($target, 0777, true))
-				{
-					$this->script->logRecord['message'] = 'Created target directory ['.$target.'].';
-					$this->script->logRecord['severity'] = 'info';
-					$this->script->log->addLog($this->script->logRecord);
-				} else {
-					$this->script->logRecord['message'] = 'Failed to create target directory ['.$target.'].';
-					$this->script->logRecord['severity'] = 'error';
-					$this->script->log->addLog($this->script->logRecord);
-					return FALSE;
-				}
-			} else {
-				$this->script->logRecord['message'] = 'Target directory ['.$target.'] exists';
-				$this->script->logRecord['severity'] = 'info';
-				$this->script->log->addLog($this->script->logRecord);
-			}
-
-			if (@ssh2_scp_recv($this->connection, $file, $target.$file))
-			{
-				$this->script->logRecord['message'] = 'File ['.$file.'] was successfully downloaded to ['.$target.'] by SCP';
-				$this->script->logRecord['severity'] = 'success';
-				$this->script->log->addLog($this->script->logRecord);
-			} else {
-				$this->script->logRecord['message'] = 'Failed to download file ['.$file.'] by SCP';
-				$this->script->logRecord['severity'] = 'error';
-				$this->script->log->addLog($this->script->logRecord);
-			}
-		}
-	}
-	
-	public function sftp_recv($file) {
-		if ($this->connection)
-		{
-			if (@!$sftp = ssh2_sftp($this->connection))
-			{
-				$this->script->logRecord['message'] = 'SFTP startup failed. File ['.$file.'] cannot be downloaded';
-				$this->script->logRecord['severity'] = 'error';
-				$this->script->log->addLog($this->script->logRecord);
-			} else {
-				$this->script->logRecord['message'] = 'SFTP startup ok.';
-				$this->script->logRecord['severity'] = 'info';
-				$this->script->log->addLog($this->script->logRecord);
-				
-				$target = rtrim($this->fileStoragePath, '/').'/'.$this->script->deviceHost.'/';
-				if (!file_exists($target))
-				{
-					if (@mkdir($target, 0777, true))
-					{
-						$this->script->logRecord['message'] = 'Created target directory ['.$target.'].';
-						$this->script->logRecord['severity'] = 'info';
-						$this->script->log->addLog($this->script->logRecord);
-					} else {
-						$this->script->logRecord['message'] = 'Failed to create target directory ['.$target.'].';
-						$this->script->logRecord['severity'] = 'error';
-						$this->script->log->addLog($this->script->logRecord);
-						return FALSE;
 					}
 				} else {
-					$this->script->logRecord['message'] = 'Target directory ['.$target.'] exists';
-					$this->script->logRecord['severity'] = 'info';
-					$this->script->log->addLog($this->script->logRecord);
-				}
-
-				@$size = filesize('ssh2.sftp://'.$sftp.'/'.$file);
-				@$stream = fopen('ssh2.sftp://'.$sftp.'/'.$file, 'r');
-				if (! $stream)
-				{
-					$this->script->logRecord['message'] = 'Failed to download file ['.$file.'] by SFTP';
-					$this->script->logRecord['severity'] = 'error';
-					$this->script->log->addLog($this->script->logRecord);
-				} else {
-					$contents = '';
-					$read = 0;
-					$len = $size;
-					while ($read < $len && ($buf = fread($stream, $len - $read)))
-					{
-						$read += strlen($buf);
-						$contents .= $buf;
-					}       
-					file_put_contents ($target.$file,$contents);
-					@fclose($stream);
-					$this->script->logRecord['message'] = 'The file ['.$file.'] of size '.$size.' bytes was successfully downloaded to ['.$target.']';
-					$this->script->logRecord['severity'] = 'success';
-					$this->script->log->addLog($this->script->logRecord);
+					$this->lastCommandError = NULL;
 				}
 			}
 		}
+		$this->logCommandErrorSeverity = self::LOG_COMMAND_ERROR_SEVERITY;
 	}
-
+	
 	public function logLastCommand() {
 		if ($this->lastCommandResult)
 		{
@@ -268,13 +226,12 @@ class SSH2 extends \Nette\Object {
 		}
 	}
 
-	public function logFingerprint()
-	{
-		if ($this->connection) {
-			$this->script->logRecord['message'] = 'Host fingerprint: ['.ssh2_fingerprint($this->connection,SSH2_FINGERPRINT_MD5 | SSH2_FINGERPRINT_HEX).']';
-			$this->script->logRecord['severity'] = 'info';
-			$this->script->log->addLog($this->script->logRecord);
-		}
+	public function sleep($sec) {
+		if(is_null($sec) || !is_numeric($sec)) $sec = 5;
+		sleep($sec);
+		$this->script->logRecord['message'] = 'Sleep for ['.$sec.'] seconds';
+		$this->script->logRecord['severity'] = 'success';
+		$this->script->log->addLog($this->script->logRecord);
 	}
 	
 }
